d3ca21abfcb9b710524415aee1fda626b0690f2d
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-11-14 20:05:08 -0500

    updated solutions, reviewed video

--- encode-word.js
+++ encode-word.js
@@ -2,13 +2,71 @@ const emojis = require('./emojis.js');
 
 
 // string-building version
+// manual version 
+// Inner Loop
+// const encodeWord = function(word) {
+//     let result = '';
+//     for (const char of word) {
+//         let found = false;
+//         for (const emoji of emojis) {
+//             if (emoji.letter === char.toLowerCase()) {
+//                 found = true;
+//                 result += emoji.symbol;
+//             }
+//         }
+
+//         if (found === false) {
+//             result += char;
+//         }
+//     }
+
+//     return result;
+// }
+
+// Double Loop 
+// Line 9 => for each character in our word go through each 
+// for (const char of word)
+// Line 10 => go through each emoji and look for the matching letter
+// for (const emoji of emojis)
+// Line 12 => when you have the matching letter append it to our results
+
+
+// mapping version 
+// does not use string building version
 const encodeWord = function(word) {
-    let result = '';
-    for (const char of word) {
+    const letters = word.split('');
+    const encodedLetters = letters.map(function(letter) {
         for (const emoji of emojis) {
-            if (emoji.letter === char) {
-                result += emoji.symbol;
+            if (emoji.letter === letter) {
+                return emoji.symbol;
             }
         }
-    }
+
+        return letter;
+    })
+
+    return encodedLetters.join('');
 }
+
+
+module.exports = encodeWord
+// When mapping over a str we can convert it by saying I want letters.
+// word.split
+// when splitting a string we make each character a separate element in an array 
+// ['d', 'o', 'g']
+
+// Line 31 is using an anonymous function
+// This function will take in one letter
+// Return matching emoji symbol
+
+// Line 33 for every emoji.letter check is the same as our character
+// If is just return emoji symbol
+
+
+
+
+// using anonymous function
+// if word: was 'dog'
+// this would be our letters
+// when we split we are taking those characters in an array.
+// emojis are two symbols 
--- main.js
+++ main.js
@@ -2,8 +2,60 @@ const translateWord = require('./translate-word.js');
 const encodeWord = require('./encode-word.js');
 
 
-const words = process.argv.slice(2);
+const command = process.argv[2];
+const words = process.argv.slice(3);
+
+if (command === 'translate') {
+    const translation = words.map(translateWord)
+    console.log(translation.join(' '));
+}   else if (command === 'encode') {
+    const encoding = words.map(encodeWord);
+    console.log(encoding.join(' '));
+}
+
+// or switch version 
+// switch is perfect if ALL of the below is true:
+// 1. you're only checking the status of ONE value
+// 2. you're checking if it EXACTLY is one of a set of values
+
+
+switch (command) {
+    case 'translate':
+        const translation = words.map(translateWord)
+        console.log(translation.join(' '));
+        break;
+    case 'encode':
+        const encoding = words.map(encodeWord);
+        console.log(encoding.join(' '));
+        break;
+    default:
+        console.log('Your choices for what to write after "node main.js" (and a space!) are:');
+        console.log('* "encode" followed by a space and then as many letters as you want');
+        console.log('* "translate" followed by a space and then as many words as you want');
+}
+
+// default is equivalent of else 
+
 // const encoding = words.map(encodeWord);
 // console.log(encoding.join(' '));
-const translation = words.map(translateWord)
-console.log(translation.join(' '));
+
+// const translation = words.map(translateWord)
+// console.log(translation.join(' '));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
